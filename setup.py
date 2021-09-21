45d2c5d8488fcb13f8968f63b8716426a9c10ee6
crewtool <45643363+crewtool@users.noreply.github.com>
2021-09-21 12:38:07 +0200

    Test github action

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='m1_api_wrapper',
-    version='2.0.0',
+    version='2.0.1',
     description='Molecule One API Wrapper',
     long_description=readme,
     long_description_content_type='text/markdown',
