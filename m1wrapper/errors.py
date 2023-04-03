e89076e6a457c10486d9cdb5d7010c00d01032ef
Szymon Pilkowski <s.pilkowski@molecule.one>
2023-04-03 16:47:48 +0200

    Ensure proper handling of HTTP errors that don't come back as JSON

--- m1wrapper/errors.py
+++ m1wrapper/errors.py
@@ -8,9 +8,12 @@ def format_error_message(error):
     else:
         return "unknown error"
 
+def is_json_response(response):
+    return response.headers.get('Content-Type').startswith('application/json')
+
 
 def maybe_handle_error(response):
-    if response.status_code >= 400 and response.status_code < 500:
+    if response.status_code >= 400 and response.status_code < 500 and is_json_response(response):
         error = format_error_message(response.json())
         raise requests.exceptions.HTTPError(error)
     else:
